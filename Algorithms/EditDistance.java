2067cda38f4ed855622f40f6ea5d1b9019b36f9a
Wayne Lu <Wayne Lu>
2016-02-15 15:26:51 -0800

    two solutions for an edit distance problem

--- Algorithms/EditDistance.java
+++ Algorithms/EditDistance.java
@@ -7,7 +7,7 @@ import java.lang.Math;
 	("abcd", "a") -> false
 	("abc", "ac") -> true
 	("abc", "ab") -> true
-        ("abc", "adc") -> ture
+   ("abc", "adc") -> ture
 
 // the normal way is using dynamic programming with recursion to get minimum edit distance of two strings 
 **/
@@ -30,37 +30,27 @@ public static boolean OneDis(String str1, String str2){
     int len1 = str1.length();
     int len2 = str2.length();
     int dis = 0;
-    if(len1 == len2){
+    if(len1 == len2){           // same length, 2 strings should be exactly the same
       for(int i=0; i<len1; i++){
          if(str1.charAt(i) != str2.charAt(i))
             dis++;
       }
-      if(dis == 1)
-        return true;
-      return false;
+      return dis == 1;
     }
-    else if( Math.abs(len1 - len2) == 1){  // one more char in one string
-      String lstring = str1;
-      String sstring = str2;
-      int l = len1, s = len2;
+    if( Math.abs(len1 - len2) == 1){  // one more char in one string
       if ( len2 > len1){
-         lstring = str2;
-         sstring = str1;
-         l = len2;
-         s = len1;
+         return OneDis(str2,str1);
       }
       int j = 0;
       for(int i=0; i<l; i++){
          if(lstring.charAt(i) == sstring.charAt(j))
             j++;
-         if(j == s)
+         if(j == s)          // including case of ("abc", "ab")
             return true;
       }
       return false;
-
     }
-    else
-       return false;
+    return false;
 
 
   }
